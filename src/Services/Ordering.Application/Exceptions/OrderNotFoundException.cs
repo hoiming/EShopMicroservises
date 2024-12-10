9a4cf63e0d939a3b2d336a81668dd9d32bda91ac
梁海明 <415429601@qq.com>
2024-12-11 00:34:28 +0800

    done  cud operation for order

---
+++ src/Services/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿
+
+using BuildingBlocks.Exceptions;
+
+namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundException : NotFoundException
+    {
+        public OrderNotFoundException(Guid id) : base("Order", id)
+        {
+            
+        }
+
+
+    }
+}
--- src/Services/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering.Application/GlobalUsing.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿global using BuildingBlocks.CQRS;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Data;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Application.Exceptions;
+global using Ordering.Domain.Models;
+global using FluentValidation;
 
-namespace Ordering.Application
-{
-    internal class GlobalUsing
-    {
-    }
-}
+ 
